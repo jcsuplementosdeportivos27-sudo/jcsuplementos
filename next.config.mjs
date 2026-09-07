9d4c082979d5b367060faf422511e748211e71e9
jcsuplementosdeportivos27-sudo <jcsuplementosdeportivos27@gmail.com>
2026-09-07 13:35:13 -0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+
+  basePath: process.env.NODE_ENV === "production"
+    ? "/jcsuplementos"
+    : "",
+
+  assetPrefix: process.env.NODE_ENV === "production"
+    ? "/jcsuplementos/"
+    : "",
+
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
